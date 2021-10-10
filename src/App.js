d971ce47fb396681eac1afe3409508f873325258
Javier <javiersar6@gmail.com>
2021-10-10 08:59:40 -0500

    Movistar girando jaja

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import logo from './LogoM.png';
+
 import './App.css';
 
 function App() {
@@ -6,20 +7,52 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hola perrooos
         </a>
-        <label for="name"> Nombre:</label>
+        <h1 class="estilo-titulo"> Generador de nombres estandar </h1>
+        <p class="h3">Seleccione tipo de recurso a crear</p>
+        <select name="Tipo de recurso">
+          <option value="Maquina">Virtual Machine</option>
+          <option value="appservice">appservice</option>
+          <option value="vmscaleset">Virtual Machine Scale set</option>
+        </select>
+        <p class="h3">Indique el nombre del proyecto</p>
+          <input type="text" id="nameproy" name="nombre" class="estilo-input" />
+        <br />
+        <p class="h3">
+          <label for="name"> Localizacion: </label>
+          <select name="Localizacion">
+            <option value="Azure">Azure</option>
+            <option value="Vmware">On-premise (VMware)</option>
+          </select>
+          <label for="name"> Entorno: </label>
+          <select name="Entorno">
+            <option value="Produccion">Produccion</option>
+            <option value="Desarrollo">Desarrollo</option>
+            <option value="Pruebas">Pruebas</option>
+          </select>
+          <label for="name"> Servicio:</label>
+          <select name="Servicio">
+            <option value="IVR">IVR</option>
+            <option value="Convergencia">Convergencia</option>
+            <option value="Pruebas">Pruebas</option>
+          </select>
+          <label for="name"> Rol: </label>
+          <select name="Rol">
+            <option value="Presentacion">Presentacion</option>
+            <option value="Aplicacion">Aplicacion</option>
+            <option value="BD">Base de datos</option>
+          </select>
+        </p>
+        <br />
+        <button class="btn btn-outline-danger">Generar</button>
       </header>
-      
+     
     </div>
     
   );
